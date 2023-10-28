b7fad3fafc72776a0aa0ad1bfca9e59d4440dd22
FedeBazan <juliopablofedericobazan@gmail.com>
2023-10-28 18:44:30 -0300

    up

---
+++ QAPablo/src/main/java/demo/guru99/com/Test.java
@@ -0,0 +1,10 @@
+package demo.guru99.com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Test {
+    WebDriver driver = new ChromeDriver();
+
+}
